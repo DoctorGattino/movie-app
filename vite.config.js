bb2a369ca77e7a586e09af212d4d841dad3b3153
Boris Zalesskii <Doctorgattino@gmail.com>
2025-03-28 07:33:28 +0300

    localhost fix allow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Слушать на всех интерфейсах
+    port: 5173, // Порт для локального сервера
+  },
 })
